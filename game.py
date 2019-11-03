18d14d30080647016340259434f8c9f939023ce5
h4sohail <hamza.sohail@carleton.ca>
2019-11-03 14:04:42 -0500

    major update, added loading and saving

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
-import random
+import random, json, os
 import matplotlib.pyplot as plt
 
 
 # Game class, has the options, a session tracker and the game state
 class Game():
-    options = ['rock', 'paper', 'scissors']  # list of choices
+    options = ['rock', 'paper', 'scissors', 'exit']  # list of choices
     sessions = 0  # tracker for amount of games played
     __state = None  # game state (this attribute is private so it can not be modified by accident)
-    session_list = [] 
+    # game data, to save and retrieve later
 
     def __init__(self):  # constructor that starts the game
         self.__state = True
@@ -18,79 +18,52 @@ class Game():
     def get_state(self):  # get the current state of the game
         return self.__state
 
-    def get_sessions(self):  # get the amount of times game has ran
-        return self.sessions
+    def save_game(self, player_data, ai_data, game_data): # method to save the player, ai and game data
+        while(True):
+            selection = input('Would you like to save the game? (y/n): ') # get the choice
+            if selection not in ['y', 'n']: # validate the input
+                print('Error 3: Incorrect input')
+                continue
+            elif selection == 'y':
+                filename = input('Please enter the filename: ')
+                try: # try/except incase file is not found
+                    os.mkdir(f'saves/{filename}') # saves under the "saves" folder
+                except FileExistsError as identifier:
+                    print(identifier)
+                    continue
 
-    def get_session_list(self):  # get the session list
-        return self.session_list
-
-    def record(self, human_choice, ai_choice, win): # method to save round data
-        round = [human_choice, ai_choice, win]
-        self.data.append(round)
-
-    def print_record(self): # method to print all previous round data
-        for i in range (0, len(self.data)):
-            print("Round # " + str(i+1) + ": " + str(self.data[i][0]) + ", " + str(self.data[i][1]) + ", " + str(self.data[i][2]) + "\n")
-
-    def print_bar_graph(self): # method to print all results of previous rounds as a horiztonal bar graph
-        human_wins = 0
-        ai_wins = 0
-        draws = 0
-        result = ''
-        for i in range(0, len(self.data)):
-            if (self.data[i][2] == "Human"):
-                human_wins += 1
-
-            elif (self.data[i][2] == "AI"):
-                ai_wins += 1
-
+                with open(f'saves/{filename}/player_data.json', 'w+') as fp:
+                    json.dump(player_data, fp) # save player data in json format
+                with open(f'saves/{filename}/ai_data.json', 'w+') as fp:
+                    json.dump(ai_data, fp) # save ai data in json format
+                with open(f'saves/{filename}/game_data.json', 'w+') as fp:
+                    json.dump(game_data, fp) # save game data in json format
+                break
             else:
-                draws += 1
-        # HUMAN WINS
-        for i in range(0,human_wins):
-            result += '#'
-        print("Human Wins   :  |" + result)
-        result = ''
-
-        # AI WINS
-        for i in range(0, ai_wins):
-            result += '#'
-
-        print("AI Wins      :  |" + result)
-        result = ''
-
-        # DRAWS
-        for i in range(0, draws):
-            result += '#'
-
-        print("Draws        :  |" + result)
-        result = ''
-
-    def save_game(self, file_name): # method to save the game data
-        f = open(file_name + '.txt', 'w')
-
-        for i in range(len(self.data)):
-            f.writelines( str(self.data[i][0]) + "," + str(self.data[i][1]) + "," + str(self.data[i][2]) + "\n")
-
-        f.close()
-
-    def load_game(self, file_name): # method to load previous game data
-        if file_name[len(file_name):len(file_name)-4:-1] == 'txt.':
-            pass
-        else:
-            file_name += '.txt'
-        f = open(file_name, 'r')
-
-        self.data = []
-
-        for line in f.readlines():
-            line = line[:len(line)-1]
-            words = line.split(",")
-            self.data.append(words)
-            print(line)
-
-        self.sessions = len(self.data)
-        f.close()        
+                break
+            
+    def load_game(self): # method to load previous game data
+        while(True):
+            selection = input('Would you like to load the game? (y/n): ') # get the choice
+            if selection not in ['y', 'n']: # validate the input
+                print('Error 2: Incorrect input')
+                continue
+            elif selection == 'y':
+                filename = input('Please enter the filename: ')
+                try: # try/except incase file is not found
+                    with open(f'saves/{filename}/player_data.json', 'r') as fp:
+                        player_data = json.load(fp) # read the player data as json
+                    with open(f'saves/{filename}/ai_data.json', 'r') as fp:
+                        ai_data = json.load(fp) # read the ai data as json
+                    with open(f'saves/{filename}/game_data.json', 'r') as fp:
+                        game_data = json.load(fp) # read the game data as json
+                except FileExistsError as identifier:
+                    print(identifier)
+                    continue
+                
+                return (player_data, ai_data, game_data)
+            else:
+                break
 
 
 # Player class, inherits from Game class
@@ -103,11 +76,8 @@ class Player(Game):
         self.wins = 0
 
     def set_choice(self):  # get the choice from the human player
-        print('Select from rock, paper or scissors')
+        print('Select from rock, paper, scissors or exit')
         self.choice = input('Input your choice: ')
-
-    def get_choice(self):  # get the player's choice
-        return self.choice
 
     def get_win_rate(self): # get the win rate as a percentage
         return int(self.win_rate[-1]*100) 
@@ -123,78 +93,81 @@ class AiPlayer(Player):
     def set_choice(self):  # set the choice for AI
         self.choice = self.options[random.randint(0, 2)]
 
+class DrawData(Game): # TO:DO class to visualize the data
+    def bar_graph(self): 
+        pass
+
 
 # main function
 def main():
     game = Game()  # initialize the Game object
     human = Player()  # initialize the Player object
     ai = AiPlayer()  # initialize the AiPlayer object
+    data = game.load_game() # ask to load the previous game (load_game() returns a tuple of 3 elements)
+    if data != None: # only load if data tuple is non empty
+        player_data = data[0] # get the player_data dictionary from tuple
+        human.name = player_data['name']
+        human.wins = player_data['wins']
+        human.win_rate = player_data['win_rate']
+        ai_data = data[1] # get the ai_data dictionary from tuple
+        ai.name = ai_data['name']
+        ai.wins = ai_data['wins']
+        ai.win_rate = ai_data['win_rate']
+        game_data = data[2] # get the game_data dictionary from tuple
+        game.sessions = game_data['sessions']
 
     while (game.get_state() == True):  # keep looping if the game state is True
-        game.sessions += 1  # track game sessions
+        
         human.set_choice()  # set human choice
 
-        if human.get_choice() not in game.options:  # check the input and break the loop if the input is not what we want
+        if human.choice not in game.options:  # check the input and break the loop if the input is not what we want
             print('Error 1: Incorrect input')
-            break
+            continue
+        
+        elif human.choice == 'exit':
+            game.close()
+            continue
 
         ai.set_choice()  # set the ai choice
 
-        print(f'{human.name} selected: {human.get_choice()}') # display the human choice
-        print(f'{ai.name} selected: {ai.get_choice()}') # display the ai choice
+        print(f'{human.name} selected: {human.choice}') # display the human choice
+        print(f'{ai.name} selected: {ai.choice}') # display the ai choice
 
         # compare the choices and display the winner
         print('------------------------')
-        if ai.get_choice() == human.get_choice():
+        if ai.choice == human.choice:
             print('[Draw]')
 
-        elif ai.get_choice() == 'rock' and human.get_choice() == 'scissors':
+        elif ai.choice == 'rock' and human.choice == 'scissors':
             print(f'[{ai.name} won]')
             ai.wins += 1
 
-        elif ai.get_choice() == 'paper' and human.get_choice() == 'rock':
+        elif ai.choice == 'paper' and human.choice == 'rock':
             print(f'[{ai.name} won]')
             ai.wins += 1
 
-        elif ai.get_choice() == 'scissors' and human.get_choice() == 'paper':
+        elif ai.choice == 'scissors' and human.choice == 'paper':
             print(f'[{ai.name} won]')
             ai.wins += 1
 
         else:
             print(f'[{human.name} won]')
             human.wins += 1
-
-        ai.win_rate.append(ai.wins / game.get_sessions()) # track the ai win rate
-        human.win_rate.append(human.wins / game.get_sessions()) # track the human win rate
-        game.session_list.append(game.get_sessions()) # track each game session in a list (used for the matplotlib graph later)
-
         print('------------------------')
-        print(f'Total sessions ran: {game.get_sessions()}')  # display total sessions ran
+        
+        game.sessions += 1  # track game sessions
+        ai.win_rate.append(ai.wins / game.sessions) # track the ai win rate
+        human.win_rate.append(human.wins / game.sessions) # track the human win rate
+
         print(f'SCORES ---- Humans: {human.wins} AI: {ai.wins}') # display the individual wins
         print(f'AI Win Rate: {ai.get_win_rate()}%') # display the ai win rate 
         print(f'HUMAN Win Rate: {human.get_win_rate()}%') # display the human win rate
-        
-        while(True):
-            exit_condition = input('Would you like to continue? y/n: ')  # get the exit condition
 
-            if exit_condition not in ['y', 'n']:  # check the input and break the loop if it is not what we want
-                print('Error 2: Incorrect input')
-                continue
-            else:
-                break
-        
-        if exit_condition == 'n':
-            plt.plot(game.session_list, ai.win_rate, color='blue')
-            plt.plot(game.session_list, human.win_rate, color='green')
-            plt.show()
-            plt.xlabel('Sessions')
-            plt.ylabel('Win Rate')
+    player_data = {"name": human.name, "wins": human.wins, "win_rate": human.win_rate}
+    ai_data = {"name": ai.name, "wins": ai.wins, "win_rate": ai.win_rate}
+    game_data = {"sessions": game.sessions}
 
-            game.close()  # close the game
-
-        else:
-            continue
-
+    game.save_game(player_data, ai_data, game_data)
 
 # entry point
 if __name__ == '__main__':
