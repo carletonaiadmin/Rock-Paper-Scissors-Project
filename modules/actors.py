d4f711c555484e6cf40061427559eb3997f52d4e
h4sohail <hamza.sohail@carleton.ca>
2019-11-04 11:04:34 -0500

    added docstrings

--- modules/actors.py
+++ modules/actors.py
@@ -5,25 +5,37 @@ from .game import Game
 class Player(Game):
     choice = None
 
-    def __init__(self):  # constructor that sets the name to 'Human'
-        self.name = 'Human'
-        self.win_rate = []
-        self.wins = 0
+    def __init__(self):  # constructor to initialize Player
+        self.name = 'Human' # set the name
+        self.win_rate = [] # win rate of all the sessions
+        self.wins = 0 # total wins
 
     def set_choice(self):  # get the choice from the human player
+        '''
+        Gets the player's choice and assigns it to the 'choice' attribute
+        '''
         print('Select from rock, paper, scissors or exit')
         self.choice = input('Input your choice: ')
 
     def get_win_rate(self): # get the win rate as a percentage
+        '''
+        Calculates the win rate of the player
+
+        Returns:
+            win_rate: Returns the winrate of the last round as a percentage
+        '''
         return int(self.win_rate[-1]*100) 
 
 
 # AiPlayer class, inherits from Player class
 class AiPlayer(Player):
-    def __init__(self):
-        self.name = 'AI'  # constructor that sets the name to 'AI'
-        self.win_rate = []
-        self.wins = 0
+    def __init__(self): # constructor to initialize AI
+        self.name = 'AI'  # set the name
+        self.win_rate = [] # win rate of all sessions
+        self.wins = 0 # total wins
 
     def set_choice(self):  # set the choice for AI
+        '''
+        Gets a random choice for the AI and assigns it to the 'choice' attribute
+        '''
         self.choice = self.options[random.randint(0, 2)]
--- modules/game.py
+++ modules/game.py
@@ -12,12 +12,30 @@ class Game():
         self.__state = True
 
     def close(self):  # method to close the game
+        '''
+        Sets the __state attribute to False
+        '''
         self.__state = False
 
     def get_state(self):  # get the current state of the game
+        '''
+        Returns the __state attribute
+        '''
         return self.__state
 
     def save_game(self, player_data, ai_data, game_data): # method to save the player, ai and game data
+        '''
+        Saves all the game info into JSON files under 'saves' sub-directory
+
+        Args:
+            player_data: The player data saved as a dictionary {"name": str, "wins": int, "win_rate": []}
+            ai_data: The AI data saved as a dictionary {"name": str, "wins": int, "win_rate": []}
+            game_data: The game data saved as a dictionary {"sessions": int}
+        Returns:
+            boolean: Returns True if the function executes, else returns False
+        Raises:
+            FileExistsError: Raises an exception if a save folder with the same name already exists
+        '''
         while(True):
             selection = input('Would you like to save the game? (y/n): ') # get the choice
             if selection not in ['y', 'n']: # validate the input
@@ -42,6 +60,17 @@ class Game():
                 return False
             
     def load_game(self): # method to load previous game data
+        '''
+        Loads all the game info from a folder under 'saves' sub-directory
+            
+        Returns:
+            player_data: The player data saved as a dictionary {"name": str, "wins": int, "win_rate": []}
+            ai_data: The AI data saved as a dictionary {"name": str, "wins": int, "win_rate": []}
+            game_data: The game data saved as a dictionary {"sessions": int}
+            boolean: Returns True if the function executes, else returns False
+        Raises:
+            FileNotFoundError: Raises an exception if a file is not found
+        '''
         while(True):
             selection = input('Would you like to load the game? (y/n): ') # get the choice
             if selection not in ['y', 'n']: # validate the input
@@ -56,7 +85,7 @@ class Game():
                         ai_data = json.load(fp) # read the ai data as json
                     with open(f'saves/{filename}/game_data.json', 'r') as fp:
                         game_data = json.load(fp) # read the game data as json
-                except FileExistsError as identifier:
+                except FileNotFoundError as identifier:
                     print(identifier)
                     continue
                 
