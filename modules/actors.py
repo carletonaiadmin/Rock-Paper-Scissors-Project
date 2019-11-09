6551a3ff474b14813d585472c166982ee405545b
h4sohail <hamza.sohail@carleton.ca>
2019-11-08 19:57:39 -0500

    implement frequency algorithm

--- modules/actors.py
+++ modules/actors.py
@@ -1,9 +1,11 @@
-import random
+import random, collections
 from .game import Game
+
 
 # Player class, inherits from Game class
 class Player(Game):
     choice = None
+    history = []
 
     def __init__(self):  # constructor to initialize Player
         self.name = 'Human' # set the name
@@ -25,6 +27,36 @@ class Player(Game):
             win_rate: Returns the winrate of the last round as a percentage
         '''
         return int(self.win_rate[-1]*100) 
+
+    def get_last_choice(self):
+        '''
+        Gets the last choice of the player
+
+        Returns:
+            last_choice: Returns the the last choice of the player
+        '''
+        last_choice = self.history[-1]
+        return last_choice
+
+    def get_all_choices(self):
+        '''
+        Gets all of the players choices
+        
+        Returns:
+            last_choice: Returns the last choice of the player
+        '''
+        last_choice = self.history
+        return last_choice
+
+    def frequent_choice(self):
+        '''
+        Gets the most frequent choice of the player
+        
+        Returns:
+            freq_choice: Returns the most frequent choice of the player
+        '''      
+        freq_choice = collections.Counter(self.history).most_common()[0][0]
+        return freq_choice
 
 
 # AiPlayer class, inherits from Player class
--- modules/draw.py
+++ modules/draw.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from .actors import AiPlayer
 
+
 class DrawData(AiPlayer): # Inheritance: AiPlayer->Player->Game
     def bar_graph(self): 
         pass
